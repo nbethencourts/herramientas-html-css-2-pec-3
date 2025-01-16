58c858ea5f870dadb7711fee308a61f780a4ad1a
nbethencourts <nbethencourts@uoc.edu>
2025-01-16 21:18:15 +0000

    Se añade la página de speakers

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-secondary-color': '#fae4b5',
+      }
+    },
   },
   plugins: [],
 }
