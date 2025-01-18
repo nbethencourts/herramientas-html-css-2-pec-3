5edb10ebd6ad4fa85e4eac1f32cc006032b42bfc
nbethencourts <nbethencourts@uoc.edu>
2025-01-18 18:21:35 +0000

    Se añaden los colores y fuentes por defecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'light-secondary-color': '#fae4b5',
-      }
+        'dark-color': '#142338', 
+        'accent-color': '#ffcc62',
+        'primary-text-emphasis': '#ffffff',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], 
+      },
     },
   },
   plugins: [],
